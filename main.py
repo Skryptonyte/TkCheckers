c4efc337d26d4ac2bfb25f5ee3b1cfc59c16a82b
Rayhan Faizel <rayhan.faizel@gmail.com>
2023-11-01 18:32:13 +0530

    Initial minimax algorithm

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from tkinter.scrolledtext import *
 
 import sqlite3
 from utils import *
+import copy
+import time
 
+DEPTH = 5
 class Checkers:
     def __init__(self):
         self.cb = \
@@ -19,7 +22,6 @@ class Checkers:
         ]
 
         self.turn = 2
-        self.selected = (-1,-1)
         self.forwarding = False
         self.forwardCell = (-1,-1)
         self.winner = None
@@ -31,6 +33,28 @@ class Checkers:
 
         self.checkWin()
 
+    def getAllMoves(self):
+        totalMoves = []
+        player = self.turn
+        i = 0
+        while (i < 8):
+            j = 0
+            while (j < 8): 
+                totalMoves += self.listMoves(player, i, j)
+                j += 1
+            i += 1
+        return totalMoves
+    def getAllJumps(self):
+        totalMoves = []
+        player = self.turn
+        i = 0
+        while (i < 8):
+            j = 0
+            while (j < 8): 
+                totalMoves += self.listJumps(player, i, j)
+                j += 1
+            i += 1
+        return totalMoves
 
     def makeMove(self,x,y,newx, newy,app):
         player = self.turn
@@ -98,6 +122,65 @@ class Checkers:
             self.takeTurn()
             app.logMessage("It is now "+NumberToName(self.turn)+"'s turn!")
         return cell
+
+    def makeMoveSilent(self,x,y,newx, newy):
+        player = self.turn
+
+        moves = []
+        totaljumps = []
+
+        # If forwarding, enumerate only viable jumps of a specific cell else enumerate all possible jumps and moves
+        if (not self.forwarding):
+            moves += self.listMoves(player,x,y)
+            
+            i = 0
+            while (i < 8):
+                j = 0
+                while (j < 8): 
+                    totaljumps += self.listJumps(player,i,j)
+                    j += 1
+                i += 1
+
+        else:
+            totaljumps += self.listJumps(player,self.forwardCell[0],self.forwardCell[1])
+        print("Possible jumps:",totaljumps)
+        print("Attempting move:",(x,y,newx,newy))
+        piece = self.cb[x][y]
+
+        cell = None
+        jumpOccured = False
+
+        # Check if jump is present, if so must take this over all else
+        if (length(totaljumps) == 0):
+            if (x,y,newx, newy) in moves:
+                self.cb[x][y] = 0
+                self.cb[newx][newy] = piece
+                res = self.promoteToKing(newx, newy)
+
+                cell = (newx, newy)
+            else:
+                return None
+        else:
+            if (x,y,newx, newy) in totaljumps:
+                self.cb[x][y] = 0
+                self.cb[newx][newy] = piece
+                self.cb[x + (newx-x)//2][y + (newy-y)//2] = 0
+                res = self.promoteToKing(newx, newy)
+
+                cell = (newx, newy)
+                jumpOccured = True
+
+            else:
+                return None
+        # If we just did a jump and we can do another jump with the same piece, we will enable forwarding for this piece only
+        if (cell and jumpOccured and length(self.listJumps(player,cell[0],cell[1])) > 0):
+            self.forwardCell = cell
+        elif cell:
+            self.forwarding = False
+            self.forwardCell = (-1,-1)
+            
+            self.takeTurn()
+        return cell
     def listMoves(self,player,x,y):
         moves = []
         piece = self.cb[x][y]
@@ -135,12 +218,12 @@ class Checkers:
             return jumps
 
         if (piece == 3 or piece == 4):
-            if (x-1 >= 0):
+            if (x-2 >= 0):
                 if (y-2 >= 0 and ( (piece == 3 and self.cb[x-1][y-1] in (2,4)) or (piece == 4 and self.cb[x-1][y-1] in (1,3)) ) and self.cb[x-2][y-2] == 0 ):
                     jumps += [(x,y,x-2,y-2)]
                 if (y+2 < 8 and ( (piece == 3 and self.cb[x-1][y+1] in (2,4)) or (piece == 4 and self.cb[x-1][y+1] in (1,3)) ) and self.cb[x-2][y+2] == 0):
                     jumps += [(x,y,x-2,y+2)]
-            if (x+1 < 8):
+            if (x+2 < 8):
                 if (y-2 >= 0 and ( (piece == 3 and self.cb[x+1][y-1] in (2,4)) or (piece == 4 and self.cb[x+1][y-1] in (1,3))) and self.cb[x+2][y-2] == 0):
                     jumps += [(x,y,x+2,y-2)]
                 if (y+2 < 8 and ( (piece == 3 and self.cb[x+1][y+1] in (2,4)) or (piece == 4 and self.cb[x+1][y+1] in (1,3)) ) and self.cb[x+2][y+2] == 0):
@@ -208,6 +291,58 @@ class Checkers:
                 self.black = "Black"
 
         return None
+    
+def doAlphaBetaSearch(instance, levels, alpha, beta, current_score,cur_depth):
+    if (instance.winner == "Black"):
+        return (None, -99999999+cur_depth)
+    if (instance.winner == "Red"):
+        return (None, 99999999-cur_depth)
+    
+    if (levels == 0):
+        return (None, current_score)
+    moves = instance.getAllMoves()
+    jumps = instance.getAllJumps()
+    best_move = None
+    old_counts = instance.countPieces()
+
+    best_score = 0
+    if (instance.turn == 1):
+        best_score = 1000000
+    else:
+        best_score = -1000000
+    if len(jumps) != 0:
+        for jump in jumps:
+            new_instance = copy.deepcopy(instance)
+            new_instance.makeMoveSilent(jump[0],jump[1],jump[2],jump[3])
+            new_counts = new_instance.countPieces()
+            if (instance.turn == 1):
+                pieces = old_counts['red'] - new_counts['red']
+                score = doAlphaBetaSearch(new_instance, levels-1,alpha,beta, current_score-pieces,cur_depth+1)[1]
+                if (score < best_score):
+                    best_move = jump
+                best_score = min(score, best_score)
+            else:
+                pieces = old_counts['black'] - new_counts['black']
+                score = doAlphaBetaSearch(new_instance, levels-1,alpha,beta, current_score+pieces,cur_depth+1)[1]
+                if (score > best_score):
+                    best_move = jump
+                best_score = max(score, best_score)
+    else:
+        for move in moves:
+            new_instance = copy.deepcopy(instance)
+            new_instance.makeMoveSilent(move[0],move[1],move[2],move[3])
+            new_counts = new_instance.countPieces()
+            if (instance.turn == 1):
+                score = doAlphaBetaSearch(new_instance, levels-1,alpha,beta, current_score,cur_depth+1)[1]
+                if (score < best_score):
+                    best_move = move
+                best_score = min(score, best_score)
+            else:
+                score = doAlphaBetaSearch(new_instance, levels-1,alpha,beta, current_score,cur_depth+1)[1]
+                if (score > best_score):
+                    best_move = move
+                best_score = max(score, best_score)
+    return (best_move, best_score)
 class CheckerApp:
     def __init__(self):
 
@@ -282,8 +417,14 @@ class CheckerApp:
             self.buttons[i*8+j]['font'] = 'Helvetica 10 underline'
         else:
             res = self.checkers.makeMove(self.selectCell[0],self.selectCell[1],i,j,self)
-
-
+            self.refreshBoard()
+            self.refreshPieceCount()
+            while (self.checkers.turn == 1 and self.checkers.winner is None):
+                (best_move, best_score) = doAlphaBetaSearch(self.checkers, DEPTH, 0,0,0,0)
+                self.logMessage("Computer making move:"+str(best_move)+"with score"+str(best_score))
+                self.checkers.makeMove(best_move[0],best_move[1],best_move[2],best_move[3],self)
+                self.refreshBoard()
+                self.refreshPieceCount()
             if (res is None):
                 self.buttons[self.selectCell[0]*8+self.selectCell[1]]['font'] = 'Helvetica 10'
                 self.selectCell = (-1,-1)
@@ -292,7 +433,7 @@ class CheckerApp:
                 self.buttons[self.selectCell[0]*8+self.selectCell[1]]['font'] = 'Helvetica 10'
                 self.selectCell = (-1,-1)
                 
-            
+
             self.refreshBoard()
             self.refreshPieceCount()
 
@@ -306,13 +447,13 @@ class CheckerApp:
                     self.buttons[i*8+j]['fg'] = 'red'
                     self.buttons[i*8+j]['text'] = 'O'
                 elif piece == 1:
-                    self.buttons[i*8+j]['fg'] = 'white'
+                    self.buttons[i*8+j]['fg'] = 'black'
                     self.buttons[i*8+j]['text'] = 'O'
                 elif piece == 4:
                     self.buttons[i*8+j]['fg'] = 'red'
                     self.buttons[i*8+j]['text'] = 'Q'
                 elif piece == 3:
-                    self.buttons[i*8+j]['fg'] = 'white'
+                    self.buttons[i*8+j]['fg'] = 'black'
                     self.buttons[i*8+j]['text'] = 'Q'               
                 else:
                     self.buttons[i*8+j]['text'] = ' '
